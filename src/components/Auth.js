35da51a3c39aae182be33685a3d0fc456af70eac
mobindas m <146184535+mobindaz@users.noreply.github.com>
2024-01-14 11:17:56 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,4 +1,4 @@
-import { auth, provider } from "../firebase-config.js";
+import { auth, googleProvider } from "../firebase-config.js"; // Assuming you have defined googleProvider in firebase-config.js
 import { signInWithPopup } from "firebase/auth";
 import "../styles/Auth.css";
 import Cookies from "universal-cookie";
@@ -8,13 +8,14 @@ const cookies = new Cookies();
 export const Auth = ({ setIsAuth }) => {
   const signInWithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, provider);
+      const result = await signInWithPopup(auth, googleProvider); // Use googleProvider here
       cookies.set("auth-token", result.user.refreshToken);
       setIsAuth(true);
     } catch (err) {
       console.error(err);
     }
   };
+
   return (
     <div className="auth">
       <p> Sign In With Google To Continue </p>
